b66a943a90a5f2ab97713d1d8602d37ef1c2e8b9
higorsouza <higor.andrade@hotmail.com>
2024-08-02 19:53:09 -0300

    adicionando classes de consumer, producer, utils e exception

---
+++ src/main/java/com/higorsouza/saga_payment_service/config/exception/ExceptionDetails.java
@@ -0,0 +1,4 @@
+package com.higorsouza.saga_payment_service.config.exception;
+
+public record ExceptionDetails(int status, String message) {
+}
---
+++ src/main/java/com/higorsouza/saga_payment_service/core/consumer/PaymentConsumer.java
@@ -0,0 +1,35 @@
+package com.higorsouza.saga_payment_service.core.consumer;
+
+import com.higorsouza.saga_payment_service.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+@AllArgsConstructor
+public class PaymentConsumer {
+
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.payment-success}"
+    )
+    public void consumerSuccessEvent(String payload) {
+        log.info("Receiving success event {} from payment-success topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.payment-fail}"
+    )
+    public void consumerFailEvent(String payload) {
+        log.info("Receiving rollback event {} from payment-fail topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+}
